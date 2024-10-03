bfe0c55eb4eea464b45a2f5afa66e1feee450ba7
liys0304 <liys0304@163.com>
2024-10-03 23:53:21 +0800

    first commit

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,17 +7,42 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let removed_str:String = input.to_string();
+    let mut begin = 0;
+    let mut end = removed_str.len();
+    for ch in removed_str.chars() {
+        if ch == ' ' {
+            begin += 1;
+        }
+        else {
+            break;
+        }
+    }
+    for ch in removed_str.chars().rev() {
+        if ch == ' ' {
+            end -= 1;
+        }
+        else {
+            break;
+        }
+    }
+    let mut s:String = removed_str[begin..end].to_string();
+    s
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let append:String = String::from(" world!");
+    let s = input.to_string();
+    let s1 = s + &append;
+    s1
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let replaced_str:String = input.to_string();
+    let new_replaced_str:String = replaced_str.replace("cars", "balloons");
+    new_replaced_str
 }
 
 #[cfg(test)]
